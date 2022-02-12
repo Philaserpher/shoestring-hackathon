0252c8c6ee745e09783e7c325e6ce56b002e337f
Philaserpher <jpfgd20@gmail.com>
2022-02-12 17:36:01 +0000

    moving graphs finished

---
+++ arduino.py
@@ -0,0 +1,29 @@
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+
+RED = [0.9412, 0.3647, 0.3647]
+S = serial.Serial("COM7", 115200)
+data = []
+time_array = []
+
+fig1 = plt.figure()
+fig1.patch.set_facecolor([0.1373, 0.1529, 0.2392])
+ax1 = fig1.add_subplot(1, 1, 1)
+ax1.set_facecolor([0.1373, 0.1529, 0.2392])
+ax1.plot(time_array, data, color=RED, linewidth=0.6)
+# ax1.axis([data.min(), data['time'].max(),
+# 0, data['CMA'].mean()+data['CMA'].std()])
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Power (W)')
+ax1.set_title('Robot Power Consumption (Cumulative Average)')
+
+
+for i in range(2):
+    S.readline()
+for i in range(100):
+    print("A")
+    time_array.append(i)
+    data.append(float((S.readline().decode())))
+    ax1.plot(time_array, data, color=RED, linewidth=0.6)
+    plt.pause(0.05)
