579eb7aace0d4bec96c560ef21e291e8851c58b4
Philaserpher <jpfgd20@gmail.com>
2022-02-13 15:58:43 +0000

    final no comments

--- arduino.py
+++ arduino.py
@@ -4,10 +4,13 @@ import numpy as np
 
 DT = 0.5
 
-RED = [0.9412, 0.3647, 0.3647]
-BLUE = [0.1960, 0.3373, 0.6588]
-GREEN = [0.1176, 0.6112, 0.2]
-BG_COLOUR = [0.1373, 0.1529, 0.2392]
+RED = [0.5294, 0, 0.0784]
+BLUE = [0.0863, 0, 0.3686]
+GREEN = [0, 0.2902, 0]
+BG_COLOUR_OLD = [0.1373, 0.1529, 0.2392]
+# BG_COLOUR = [0.3216, 0.3686, 0.4392]
+BG_COLOUR = [0.2549, 0.2902, 0.3608]
+WHITE = [1, 1, 1]
 
 S = serial.Serial("COM7", 115200)  # initialise serial at 115200 baud rate
 # create a dataframe
@@ -17,38 +20,36 @@ DATA = [np.array([]), np.array([]), np.array([]), np.array([])]
 # create out figure
 fig1 = plt.figure(figsize=(19, 10))
 fig1.patch.set_facecolor(BG_COLOUR)
-
+fig1.subplots_adjust(hspace=0.5, wspace=0.25)
 # set up our first plot
 AX1 = fig1.add_subplot(2, 2, 1)
 AX1.set_facecolor(BG_COLOUR)
 AX1.set_xlabel('Time (s)')
-AX1.set_ylabel('Power (W)')
+AX1.set_ylabel('Power (mW)')
 AX1.set_title('Fan Power Consumption')
 # raw data vs time
 
 
 def plot_raw(ax1, data):
-    g1, = ax1.plot(data[0], data[1], color=RED, linewidth=0.6)
-    g2, = ax1.plot(data[0], data[2], color=BLUE, linewidth=0.6)
-    leg = ax1.legend([g1, g2], ['Raw Data', '5 Second Average'],
-                     loc='lower right', facecolor=BG_COLOUR)
-    for text in leg.get_texts():
-        text.set_color(RED)
+    g1, = ax1.plot(data[0], data[1], color=RED, linewidth=1)
+    g2, = ax1.plot(data[0], data[2], color=BLUE, linewidth=1)
+    ax1.legend([g1, g2], ['Raw Data', '5 Second Average'],
+               loc='lower right', facecolor=BG_COLOUR)
 
 
 # set up second plot
 AX2 = fig1.add_subplot(2, 2, 2)
 AX2.set_facecolor(BG_COLOUR)
 AX2.set_xlabel('Time (s)')
-AX2.set_ylabel('Power (W)')
+AX2.set_ylabel('Power (mW)')
 AX2.set_title('Fan Power Consumption (Cumulative Average)')
 
 
 def plot_cumulative(ax2, data, max, max_pos, annotation, power_usage,
                     power_usage_total):
-    g1, = ax2.plot(data[0], data[3], color=RED, linewidth=0.6)
-    g2, = ax2.plot(data[0], data[3]+5, color=BLUE, linewidth=0.6)
-    g3, = ax2.plot(data[0], data[3]-5, color=GREEN, linewidth=0.6)
+    g1, = ax2.plot(data[0], data[3], color=RED, linewidth=1)
+    g2, = ax2.plot(data[0], data[3]+5, color=BLUE, linewidth=1)
+    g3, = ax2.plot(data[0], data[3]-5, color=GREEN, linewidth=1)
     ax2.legend([g2, g1, g3],
                ['Upper Bound', 'Cumulative Average', 'Lower Bound'],
                loc='lower left', facecolor=BG_COLOUR)
@@ -62,9 +63,9 @@ def plot_cumulative(ax2, data, max, max_pos, annotation, power_usage,
         max = int(data[3][-1]+5)
         max_pos = data[0][-1]
         max_plot = round(max*0.001, 3)
-        annotation = ax2.annotate(f"{max_plot} W", (max_pos, max+20), size=14, ha='right',
-                                  va='top', bbox=dict(boxstyle='round',
-                                                      fc=BG_COLOUR))
+        annotation = ax2.annotate(f"{max_plot} W", (max_pos, max+20), size=14,
+                                  ha='right', va='top',
+                                  bbox=dict(boxstyle='round', fc=BG_COLOUR))
     average_rate_hour = int(data[3][-1]*3.6)
     try:
         power_usage.remove()
@@ -87,14 +88,28 @@ def plot_cumulative(ax2, data, max, max_pos, annotation, power_usage,
 
 AX3 = fig1.add_subplot(2, 2, 3)
 AX3.set_facecolor(BG_COLOUR)
-AX3.set_xlabel('Time (s)')
-AX3.set_ylabel('Power (W)')
-AX3.set_title('Fan Power Consumption (Box Plot)')
+AX3.set_xlabel('Frequency (Hz)')
+AX3.set_ylabel('Power (mW)')
+AX3.set_title('Fourier Transform of Power Data')
 
 
 def plot_box(ax3, data):
     ax3.cla()
     ax3.boxplot(data[1])
+
+
+AX4 = fig1.add_subplot(2, 2, 4)
+AX4.set_facecolor(BG_COLOUR)
+AX4.set_xlabel('Time (s)')
+AX4.set_ylabel('Power (mW)')
+AX4.set_title('Fan Power Consumption (Box Plot)')
+
+
+def plot_fft(ax4, data):
+    ax4.cla()
+    ax4.plot(data[0], data[1], color=RED, linewidth=1)
+    ax4.axis([0, data[0].max(),
+              0, data[1].max()+50])
 
 
 counter = 0.0
@@ -122,17 +137,18 @@ while True:
     DATA[2] = np.append(DATA[2], five_second_average)
     DATA[3] = np.append(DATA[3], cumulative_average)
 
+    if counter % 5 == 0:
+        filtered_power = DATA[2]*np.hanning(len(DATA[2]))
+        filtered_power = np.absolute(np.fft.fft(filtered_power))
+        fvals = np.fft.fftfreq(len(filtered_power), d=1/400)
+        filtered_power = np.fft.fftshift(filtered_power)
+        fvals = np.fft.fftshift(fvals)
+
     plot_raw(AX1, DATA)
     MAX, MAX_POS, ANNOTATION, POWER_USAGE, POWER_USAGE_TOTAL = plot_cumulative(
         AX2, DATA, MAX, MAX_POS, ANNOTATION, POWER_USAGE, POWER_USAGE_TOTAL)
     plot_box(AX3, DATA)
+    plot_fft(AX4, [fvals, filtered_power])
     counter += DT
+
     plt.pause(0.5)
-
-
-
-filtered_power = data[2]*np.hanning(len(data[2]))
-for i in range(5):
-    filtered_power[i] = 0
-filtered_power = (np.fft.fft(filtered_power)).abs()
-fvals = np.fft.fftfreq(len(filtered_power), d=1/3600)
